0e40b8a4cc76a12d9837d5d8e2df0bc4bac68a98
Isabella Ramos <isabellamenezesramos@outlook.com>
2021-04-16 14:53:47 -0300

    add DAO - GrupoAtividade (falta adicionar/remover usuario)

---
+++ src/dao/GrupoAtividadeDAO.java
@@ -0,0 +1,74 @@
+package dao;
+
+import java.util.List;
+import model.Colaborador;
+import model.DiaSemana;
+import model.GrupoAtividade;
+
+public class GrupoAtividadeDAO {
+    private static List<GrupoAtividade> listaGrupoAtividades;
+    
+    public boolean novoGrupo(int idGrupo,String nomeAtividade, String duracao, String horaInicio, DiaSemana diaSemana,
+                           String descricaoAtividade, UsuarioDAO listaUsuarios,
+                            Colaborador funcionarioResponsavel){
+        GrupoAtividade gaNovo = new GrupoAtividade(idGrupo, nomeAtividade, duracao, horaInicio, diaSemana, descricaoAtividade, listaUsuarios, funcionarioResponsavel);
+        
+        if(idExists(idGrupo)){
+            return false;
+        }
+        else{
+            listaGrupoAtividades.add(gaNovo);
+            return true;
+        }
+    }
+    
+    private static boolean idExists(int id) {
+        for(GrupoAtividade g : listaGrupoAtividades) {
+            if(g.getIdGrupo() == id){
+                return true;
+            }
+        }
+        for(GrupoAtividade g : GrupoAtividadeDAO.listaGrupoAtividades) {
+            if(g.getIdGrupo()== id) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void alterarDuracao(int idGrupo, String novaDuracao){
+        for(GrupoAtividade g :listaGrupoAtividades){
+            if(g.getIdGrupo() == idGrupo){
+                g.setDuracao(novaDuracao);
+            }
+        }
+    }
+    
+    public void alterarHoraInicio(int idGrupo, String novaHoraInicio){
+        for(GrupoAtividade g :listaGrupoAtividades){
+            if(g.getIdGrupo() == idGrupo){
+                g.setHoraInicio(novaHoraInicio);
+            }
+        }
+    }
+    
+    public void alterarHoraInicio(int idGrupo, DiaSemana novoDiaSemana){
+        for(GrupoAtividade g :listaGrupoAtividades){
+            if(g.getIdGrupo() == idGrupo){
+                g.setDiaSemana(novoDiaSemana);
+            }
+        }
+    }
+    
+    public void verAtividade(int idGrupo){
+        for(GrupoAtividade g :listaGrupoAtividades){
+            if(g.getIdGrupo() == idGrupo){
+                g.toString();
+            }
+        }
+    }
+    
+    public List<GrupoAtividade> listarGrupoAtividade(){
+        return listaGrupoAtividades;
+    }
+}
--- src/model/GrupoAtividade.java
+++ src/model/GrupoAtividade.java
@@ -4,52 +4,30 @@ package model;
 import dao.UsuarioDAO;
 
 public class GrupoAtividade {
-    private String nomeAtividade;
+    private int idGrupo;
+    private String nomeGrupo;
     private String duracao;
     private String horaInicio;
     private DiaSemana diaSemana;
-    private String descricaoAtividade;
+    private String descricaoGrupo;
     private UsuarioDAO listaUsuarios;
     private Colaborador funcionarioResponsavel;
     
-    public GrupoAtividade(String nomeAtividade, String duracao, String horaInicio, DiaSemana diaSemana,
+    public GrupoAtividade(int idGrupo,String nomeAtividade, String duracao, String horaInicio, DiaSemana diaSemana,
                            String descricaoAtividade, UsuarioDAO listaUsuarios,
                             Colaborador funcionarioResponsavel){
-        this.descricaoAtividade = descricaoAtividade;
-        this.nomeAtividade = nomeAtividade;
+        this.descricaoGrupo = descricaoAtividade;
+        this.nomeGrupo = nomeAtividade;
         this.duracao = duracao;
         this.diaSemana = diaSemana;
         this.horaInicio = horaInicio;
         this.listaUsuarios = listaUsuarios;
-        this.funcionarioResponsavel = funcionarioResponsavel;     
+        this.funcionarioResponsavel = funcionarioResponsavel; 
+        this.idGrupo = idGrupo;
     }
 
-    public String getNomeAtividade() {
-        return this.nomeAtividade;
-    }
-
-    public String getDuracao() {
-        return this.duracao;
-    }
-
-    public String getHoraInicio() {
-        return this.horaInicio;
-    }
-
-    public DiaSemana getDiaSemana() {
-        return this.diaSemana;
-    }
-
-    public String getDescricaoAtividade() {
-        return this.descricaoAtividade;
-    }
-
-    public UsuarioDAO getListaUsuarios() {
-        return this.listaUsuarios;
-    }
-
-    public Colaborador getFuncionarioResponsavel() {
-        return this.funcionarioResponsavel;
+    public int getIdGrupo() {
+        return idGrupo;
     }
     
     public void setDiaSemana(DiaSemana diaSemana){
@@ -66,10 +44,10 @@ public class GrupoAtividade {
 
     @Override
     public String toString() {
-        return "GrupoAtividade{" + "nomeAtividade=" + nomeAtividade +
+        return "GrupoAtividade{" + "nomeAtividade=" + nomeGrupo +
                 ", duracao=" + duracao + ", horaInicio=" + horaInicio + 
                 ", diaSemana=" + diaSemana + ", descricaoAtividade=" + 
-                descricaoAtividade + ", listaUsuarios=" + listaUsuarios + 
+                descricaoGrupo + ", listaUsuarios=" + listaUsuarios + 
                 ", funcionarioResponsavel=" + funcionarioResponsavel + '}';
     }
 
