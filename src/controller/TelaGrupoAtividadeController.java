712fa1201becfebd00693a68bcc7109fa38b0a83
Isabella Ramos <isabellamenezesramos@outlook.com>
2021-05-14 18:46:15 -0300

    adicionando tela QuadroHorario

--- src/controller/TelaGrupoAtividadeController.java
+++ src/controller/TelaGrupoAtividadeController.java
@@ -7,7 +7,15 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,6 +24,43 @@ import javafx.fxml.Initializable;
  */
 public class TelaGrupoAtividadeController implements Initializable {
 
+    @FXML
+    private TextField edtID;
+    @FXML
+    private TextField edtNome;
+    @FXML
+    private TextField edtDuracao;
+    @FXML
+    private TextField edtHoraInicio;
+    @FXML
+    private TextField edtIDColaborador;
+    @FXML
+    private ChoiceBox<?> cbDiaSemana;
+    @FXML
+    private TextField edtDescricao;
+    @FXML
+    private Button btnInserir;
+    @FXML
+    private Button btnAtualizar;
+    @FXML
+    private Button btnExcluir;
+    @FXML
+    private TableView<?> tableGrupoAtividades;
+    @FXML
+    private TableColumn<?, ?> columnIDCol;
+    @FXML
+    private TableColumn<?, ?> columnNomeCol;
+    @FXML
+    private TableColumn<?, ?> columnCPFCol;
+    @FXML
+    private TableColumn<?, ?> columnSalarioCol;
+    @FXML
+    private TableColumn<?, ?> columnCargaHorariaCol;
+    @FXML
+    private TableColumn<?, ?> columnDataNascimentoCol;
+    @FXML
+    private TableColumn<?, ?> columnTipoCol;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +68,21 @@ public class TelaGrupoAtividadeController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnInserirClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnAtualizarClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnExcluirClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void tableColaboradoresClick(MouseEvent event) {
+    }
     
 }
--- src/controller/TelaQuadroHorarioController.java
+++ src/controller/TelaQuadroHorarioController.java
@@ -5,12 +5,20 @@
  */
 package controller;
 
+import dao.QuadroHorario;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import model.DiaSemana;
+import model.ItemQuadroHorario;
 
 /**
  * FXML Controller class
@@ -21,6 +29,24 @@ public class TelaQuadroHorarioController implements Initializable {
 
     @FXML
     private Button btnVoltar;
+    @FXML
+    private TableColumn<ItemQuadroHorario, Integer> columnID;
+    @FXML
+    private TableColumn<ItemQuadroHorario, String> columnNome;
+    @FXML
+    private TableColumn<ItemQuadroHorario, String> columnDescricao;
+    @FXML
+    private TableColumn<ItemQuadroHorario, Integer> columnHora;
+    @FXML
+    private TableColumn<ItemQuadroHorario, DiaSemana> columnDIa;
+    @FXML
+    private TableColumn<ItemQuadroHorario, Integer> columnDuracao;
+    @FXML
+    private TableColumn<ItemQuadroHorario, ?> columnUsuarios;
+    @FXML
+    private TableColumn<ItemQuadroHorario, ?> columnColaboradores;
+    @FXML
+    private TableView<ItemQuadroHorario> tableQuadroHorario;
 
     /**
      * Initializes the controller class.
@@ -28,11 +54,28 @@ public class TelaQuadroHorarioController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        this.carregaTabela();
     }    
 
     @FXML
     private void btnVoltarClick(ActionEvent event) {
         ProjetoPOO.TrocaTela("inicialColaborador");
     }
-    
+
+    @FXML
+    private void tableCoordenadoresClick(MouseEvent event) {
+    }
+        
+    private void carregaTabela() {
+        try {
+            this.tableQuadroHorario.getItems().clear();
+            this.tableQuadroHorario.getItems().clear();
+        } catch (Exception erro) {
+        }
+        
+        ObservableList observableList = FXCollections.observableArrayList(QuadroHorario.visualizarQuadroHorario());
+        this.tableQuadroHorario.setItems(observableList);
+        ObservableList observableList2 = FXCollections.observableArrayList(QuadroHorario.visualizarQuadroHorario());
+        this.tableQuadroHorario.setItems(observableList2);
+    }
 }
